fe8d3608b859fbeb89856d1dec50bfc40709b86b
wubmu <923185028@qq.com>
2022-10-06 00:20:17 +0800

    框架搭建

---
+++ mall-tiny-01/src/main/java/com/example/mall/tiny/config/MyBatisConfig.java
@@ -0,0 +1,11 @@
+package com.example.mall.tiny.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+/*
+* MyBatis配置类
+* */
+@Configuration
+@MapperScan("com.example.mall.tiny.mbg.mapper")
+public class MyBatisConfig {
+}
